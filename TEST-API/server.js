95e5741316c9dc3484f22868d623de63b7dc7c86
Ebener <ebener.santos@gmail.com>
2026-04-12 23:25:42 -0300

    iniciado mockup backend

---
+++ TEST-API/server.js
@@ -0,0 +1,14 @@
+const jsonServer = Require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+// port
+const port = 4000;
+
+// create server
+server.use(middlewares);
+server.use(router);
+server.listen(port, () => {
+    console.log(`JSON Server is running at ${port} 🎉`);
+});
